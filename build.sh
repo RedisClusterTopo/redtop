3b112d8c423280d1a6381b0a1dc8efc992642ec7
alkief <akiefer6@gmail.com>
2017-01-02 20:15:03 -0500

    Rewrite gulp to build from local by default

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
+#!/bin/sh
 
 gulp clean
 gulp cloneAssets
-gulp
+gulp git-build
--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,9 @@ var runSequence = require('run-sequence');
 
 
 
-gulp.task('default', ['client', 'server', 'test']);
+gulp.task('default', ['local']);
+
+gulp.task('git-build', ['client', 'server', 'test']);
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -34,17 +36,22 @@ gulp.task('client', ['cli-objects', 'cli-scripts']);
 
 gulp.task('cli-objects', function(){
   return gulp.src('assets/client/src/js/objects/*.js')
-      .pipe(gulp.dest('lib/js/client-objects/'));
+      .pipe(gulp.dest('lib/js/'));
 });
 
 gulp.task('cli-scripts', function(){
   return gulp.src('assets/client/src/js/scripts/*.js')
-      .pipe(gulp.dest('lib/js/client-scripts/'));
+      .pipe(gulp.dest('lib/js/'));
 });
 
 ////////////////////////////////////////////////////////////////////////////////
 
-gulp.task('server', ['public', 'css', 'server-scripts']);
+gulp.task('server', ['app-main', 'public', 'css', 'server-scripts']);
+
+gulp.task('app-main', function(){
+  return gulp.src('assets/server/src/server.js')
+      .pipe(gulp.dest('lib/'));
+});
 
 gulp.task('public', function(){
   return gulp.src('assets/server/public/*.html')
@@ -60,13 +67,13 @@ gulp.task('server-scripts', function(){
   gulp.src('assets/server/src/server.js')
   .pipe(gulp.dest('lib/'));
   return gulp.src('assets/server/src/js/*.js')
-      .pipe(gulp.dest('lib/js/server-scripts'));
+      .pipe(gulp.dest('lib/js/'));
 });
 
 ////////////////////////////////////////////////////////////////////////////////
 
 gulp.task('test', function(){
-  return gulp.src('assets/client/test/*')
+  return gulp.src('assets/server/test/*')
       .pipe(gulp.dest('lib/test'));
 });
 
@@ -86,4 +93,52 @@ gulp.task('cleanSrc', function(){
 
 gulp.task('cleanPublic', function(){
   return del(['public'], {force: true});
+});
+
+
+
+
+//This build option assumes the three project repos (redtop (called 'main' below), server and client)
+//are in the same directory
+
+//Use this option for testing changes made without pushing to github
+gulp.task('local', ['l-client', 'l-server', 'l-test']);
+
+gulp.task('l-client', ['l-cli-objects', 'l-cli-scripts']);
+
+gulp.task('l-cli-objects', function(){
+  return gulp.src('../client/src/js/objects/*.js')
+      .pipe(gulp.dest('lib/js/'));
+});
+
+gulp.task('l-cli-scripts', function(){
+  return gulp.src('../client/src/js/scripts/*.js')
+      .pipe(gulp.dest('lib/js/'));
+});
+
+gulp.task('l-server', ['l-app-main', 'l-public', 'l-css', 'l-server-scripts']);
+
+gulp.task('l-app-main', function(){
+  return gulp.src('../server/src/server.js')
+      .pipe(gulp.dest('lib/'));
+});
+
+gulp.task('l-public', function(){
+  return gulp.src('../server/public/*.html')
+      .pipe(gulp.dest('public/'));
+});
+
+gulp.task('l-css', function(){
+  return gulp.src('../server/src/css/*.css')
+      .pipe(gulp.dest('lib/css'));
+});
+
+gulp.task('l-server-scripts', function(){
+  return gulp.src('../server/src/js/*.js')
+      .pipe(gulp.dest('lib/js/'));
+});
+
+gulp.task('l-test', function(){
+  return gulp.src('../server/test/*')
+      .pipe(gulp.dest('lib/test'));
 });
